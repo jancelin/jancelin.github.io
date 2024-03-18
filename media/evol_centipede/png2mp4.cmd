8bf469a1cd42de904c3b51f094932f33d540b887
jancelin <julien.ancelin@inra.fr>
2024-03-18 18:05:18 +0100

    upd video evolution

--- media/evol_centipede/png2mp4.cmd
+++ media/evol_centipede/png2mp4.cmd
@@ -1,1 +1,1 @@
-ffmpeg -framerate 1 -pattern_type glob -i '*.png' -c:v libx264 -r 1 -pix_fmt yuv420p output.mp4
+ffmpeg -framerate 1 -pattern_type glob -i '*.png' -c:v libx264 -r 1 -pix_fmt yuv420p  -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2"  evolution_centipedeRTK0324.mp4
